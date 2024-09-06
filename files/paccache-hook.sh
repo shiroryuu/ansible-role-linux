34a0367ddb9c9c859ce5bc8ecbd450b48b873ee5
Arvind H Nair <arvdev96@gmail.com>
2024-09-06 18:16:03 +0530

    Add pacman contrib and paccache hook
    Signed-off-by: Arvind H Nair <arvdev96@gmail.com>

---
+++ files/paccache-hook.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+. /etc/paccache-hook.conf
+
+cache_args=""
+for cdir in ${cache_dirs[@]}; do
+	cache_args="$cache_args -c $cdir"
+done
+
+if [ "$installed" = "true" ]; then
+	echo "Removing old installed packages..."
+	if [ -n "$installed_move_to" ]; then
+		paccache $cache_args -m "$installed_move_to" "-k${installed_keep:-2}" $extra_args $installed_extra_args
+	else
+		paccache $cache_args "-rk${installed_keep:-2}" $extra_args $installed_extra_args
+	fi
+fi
+
+if [ "$uninstalled" = "true" ]; then
+	echo "Removing old uninstalled packages..."
+	if [ -n "$uninstalled_move_to" ]; then
+		paccache $cache_args -m "$uninstalled_move_to" "-uk${uninstalled_keep:-0}" $extra_args $uninstalled_extra_args
+	else
+		paccache $cache_args "-ruk${uninstalled_keep:-0}" $extra_args $uninstalled_extra_args
+	fi
+fi
+
